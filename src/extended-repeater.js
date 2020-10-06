1d14fe067bde061c9b0552112e0c90ed80736865
omaslov <omaslov@xbsoftware.by>
2020-10-06 16:05:52 +0300

    dev: add solution to extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+  if (options) {
+    let substring = [];
+    let string = [];
+    if (options.addition !== undefined) {
+      let repeat = options.additionRepeatTimes || 1;
+      for (let i = 0; i < repeat; i++) {
+        substring.push(String(options.addition));
+      }
+      substring = substring.join(options.additionSeparator || '|')
+     
+    }
+    let repeat = options.repeatTimes || 1;
+    for (let i = 0; i < repeat; i++) {
+      string.push(String(str).concat(substring))
+    }
+    return string.join(options.separator || '+')
+  }
+  return String(str);
 };
   
