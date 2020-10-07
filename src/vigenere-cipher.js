af7c31367bddee4fe3029e1fe5c7a3bcfda594a1
omaslov <omaslov@xbsoftware.by>
2020-10-07 13:41:26 +0300

    dev: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(straight = true) {
+    this.straight = straight;
+  }
+  encrypt(message, key, straight) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let res = [];
+    let counter = 0;
+    if (!message || !key) throw new Error();
+    for (let i = 0; i < message.length; i++) {
+      if (counter > key.length - 1) { counter = 0 };
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        res.push(message[i].charCodeAt())
+      }
+      else {
+        res.push((message[i].charCodeAt() + key[counter].charCodeAt()) % 26 + 65);
+        counter++
+      }
+    }
+    return this.straight ? String.fromCharCode(...res) : String.fromCharCode(...res.reverse());
+  }
+  decrypt(message, key, straight) {
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let res = [];
+    let counter = 0;
+    if (!message || !key) throw new Error();
+    for (let i = 0; i < message.length; i++) {
+      if (counter > key.length - 1) { counter = 0 };
+      if (message[i].charCodeAt() < 65 || message[i].charCodeAt() > 90) {
+        res.push(message[i].charCodeAt())
+      } else {
+        res.push((message[i].charCodeAt() - key[counter].charCodeAt() + 26) % 26 + 65);
+        counter++;
+      }
+    }
+    return this.straight ? String.fromCharCode(...res) : String.fromCharCode(...res.reverse());
   }
 }
 
